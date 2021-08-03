d4e1b877cafbd5c360e7b2314da31ceaf258675c
Shivam Tyagi <shivamtyagicool@gmail.com>
2021-08-03 23:50:54 +0530

    Login and signup page popover work complete

--- src/app/components/user-profile/profile-poper/profile-poper.component.ts
+++ src/app/components/user-profile/profile-poper/profile-poper.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { LoginComponent } from '../../auth/login/login.component';
+import { SignupComponent } from '../../auth/signup/signup.component';
 
 @Component({
   selector: 'app-profile-poper',
@@ -16,15 +18,35 @@ export class ProfilePoperComponent implements OnInit {
     this.popoverController.dismiss();
   }
 
-  redirectToLoginPage(){
+  async redirectToLoginPage(ev:any){
     console.log('inside m');
     this.ClosePopover();
-    this.router.navigate(['/login']);
-
+    // this.router.navigate(['/login']);
+    return await this.openLoginPopover(ev);
   }
 
-  redirectToSignupPage(){
+  private async openLoginPopover(ev: any) {
+    const popover = await this.popoverController.create({
+      component: LoginComponent,
+      event: ev,
+      translucent: true
+    });
+    const currentPopover = popover;
+    return popover.present();
+  }
+
+  async redirectToSignupPage(ev:any){
     this.ClosePopover();
-    this.router.navigate(['/signup']);
+    // this.router.navigate(['/signup']);
+    return await this.openSignUpPopover(ev);
+  }
+  private async openSignUpPopover(ev: any) {
+    const popover = await this.popoverController.create({
+      component: SignupComponent,
+      event: ev,
+      translucent: true
+    });
+    const currentPopover = popover;
+    return popover.present();
   }
 }
